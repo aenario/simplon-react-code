ffc25feb4b47c18275fc16e88ef913395db5faa7
Romain <romain@cozycloud.cc>
2016-07-05 16:35:30 +0200

    state

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,7 +1,25 @@
 import React from "react"
 
 export default class HelloWorld extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {counter: 0}
+    }
+
+    onTimer(){
+        this.setState({counter: this.state.counter + 1});
+    }
+
+    componentWillMount(){
+        this.timer = setInterval(this.onTimer.bind(this), 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+
     render(){
-        return <p>Helo {this.props.who} !</p>;
+        return <p>Helo {this.props.who} ! ({this.state.counter})</p>;
     }
 }
